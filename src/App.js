3907bf3b5f6b951616080ab5e07feb17856bb84d
Codenation12 <Codenation12@codenation12s-MacBook-Pro.local>
2019-06-25 14:22:47 +0100

    grid headers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Grid from './Grid';
 import Title from './common/Title';
+import GridHeader from './common/GridHeader';
 
 function App() {
   return (
     <div className="App">
       <Title/>
       <Grid/>
+      <GridHeader />
     </div>
   );
 }
