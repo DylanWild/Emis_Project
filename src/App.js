331d3f59081f4bf7d14bc9c2c92988e56e85aaba
Dylan Wild <wilddylan@gmail.com>
2019-06-26 14:56:29 +0100

    Rows complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Grid from './Grid';
+// import Grid from './Grid';
 import Title from './common/Title';
+import GridObj from '../src/components/Time';
 
 function App() {
   return (
     <div className="App">
       <Title/>
-      <Grid/>
+      {/* <Grid/> */}
+      <GridObj/>
     </div>
   );
 }
--- src/Grid.js
+++ src/Grid.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './Grid.css';
+// import GridObj from './Time';
+import GridRow from './components/GridRow'
 
 export class Grid extends Component {
+
+   
     render() {
         return (
-            <div className="grid-container">
-                
+            <div className="grid-container">  
             </div>
+            
+       
         )
+
     }
 }
 
