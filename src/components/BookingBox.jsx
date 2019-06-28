4f88351124f4d9e1ca6a9c623218f96e80529605
Codenation12 <Codenation12@codenation12s-MacBook-Pro.local>
2019-06-28 10:53:59 +0100

    border box style changes

--- src/components/BookingBox.jsx
+++ src/components/BookingBox.jsx
@@ -8,25 +8,34 @@ export default class BookingBox extends Component {
         reason: "",
         notes: ""
     }
+    input(){
+        this.setState({firstname:document.getElementById('firstName').input},
+                      {surname:document.getElementById('surname').input},
+                      {reason:document.getElementById('reasonInput').input},
+                      {notes:document.getElementById('text').input})
+    }
     render() {
         return (
             <div className="bookingBox" id = "bookingBox">
-            <h1 className='text'>Book a Slot</h1>
-            <p className="name">Patient Name</p>
-                <input className="firstName inputs" type="text" placeholder="First Name" required="yes"></input>
-                <input className="surname inputs" type="text" placeholder="Second Name" required="yes"></input>
-                <p className="reasonText text">Reason</p>
-                <textarea className="reasonInput inputs" type="text" required="yes"></textarea>
-                <p className="notesText text" >Notes</p>
-                <textarea type="text inputs" rows="3" ></textarea>
+                <div className="booking-area">
+                    <h1 className='text'>Book a Slot</h1>
+                    <p className="name">Patient Name</p>
+                    <input className="inputs" id="firstName" type="text" placeholder="First Name" required="yes"></input>
+                    <input className="inputs" id="surname" type="text" placeholder="Second Name" required="yes"></input>
+                    <p className="reasonText text">Reason</p>
+                    <textarea className="inputs" id="reasonInput" type="text" required="yes"></textarea>
+                    <p className="notesText text" >Notes</p>
+                    <textarea type="inputs" rows="3" id="text" ></textarea>
+                </div>
                 <div className="buttons">
-                <button className="buts" id="cancelButton">Cancel</button>
-                <button className="buts" id="submitButton">Submit</button>
+                    <button className="buts" id="cancelButton">Cancel</button>
+                    <button className="buts" id="submitButton">Submit</button>
                 </div>
             </div>
         )
     }
 }
+
 
 // Textbox field for patient name - this must be split into firstname and surname.
 // Field label for patient name is : "Patient Name"
--- src/components/OnClickBox.jsx
+++ src/components/OnClickBox.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import GridRow from './GridRow'
-import ReactDOM from 'react-dom'
+// import GridRow from './GridRow'
+// import ReactDOM from 'react-dom'
 
 export default class Clicky extends Component {
 
